2f4062f406390ef9ea6c5be566a999ee99392681
Jean-François Dessureault <jfdess@gmail.com>
2025-05-01 21:44:01 -0400

    deps: upgrade keymap-drawer@0.22.0 python@3.13

--- flake.nix
+++ flake.nix
@@ -6,9 +6,9 @@
   outputs = { nixpkgs, ... }: {
     devShells.x86_64-linux.default = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      keymap-drawer_version = "0.20.0";
+      keymap-drawer_version = "0.22.0";
     in pkgs.mkShell {
-      packages = with pkgs; [ python312 uv ];
+      packages = with pkgs; [ python313 uv ];
 
       shellHook = ''
         uv venv
